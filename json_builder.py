985e82edec1373ac0202d1be7d0f4e0f1a4a7efb
KC-HaroldLee <kklastchnpd@naver.com>
2024-08-01 07:49:21 +0900

    질문 제작 파일 작성

--- json_builder.py
+++ json_builder.py
@@ -1,7 +1,7 @@
 from lib.constance import *
 import json
 
-TARGET_TXT = './data_sample/poll.txt'
+TARGET_TXT = 'data_sample/004_편차_좁힌_버전_젠더감수성_테스트_(20문항).txt'
 
 lines = [line.replace('\n','') for line in open(TARGET_TXT, 'r').readlines()]
 json_data = json_base_dict
@@ -19,10 +19,10 @@ for line_num, line in enumerate(lines) :
             "value": 2,
             "text": "그렇다"
             },
-            {
-            "value": 1,
-            "text": "잘 모르겠다"
-            },
+            # {
+            # "value": 1,
+            # "text": "잘 모르겠다"
+            # },
             {
             "value": 0,
             "text": "아니다"
