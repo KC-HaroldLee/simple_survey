51a38a14c28d17f9c6aa820632e41dc86e36837f
KC-HaroldLee <kklastchnpd@naver.com>
2024-08-01 08:04:42 +0900

    자동저장 반영

--- src/json.js
+++ src/json.js
@@ -1,8 +1,8 @@
 import data from "./page"
 
 export const json = {
-    "title": "설문조사!",
-    "description": "This form is designed to report issues and propose suggestions for improvement. Your feedback is invaluable in helping us make our services better and resolve issues promptly. Please complete the form below in as much detail as possible.",
+    "title": "설문조사",
+    "description": "{설명1이 들어갈 부분}",
     "completedHtml": "<div style=\"max-width:688px;text-align:center;margin:16px auto;\">\n\n<div style=\"padding:0 24px;\">\n<h4>Thank you.</h4>\n<p>Your message has been sent successfully.</p>\n</div>\n\n</div>\n\n",
     "pages": [data],
     "showQuestionNumbers": "off",
